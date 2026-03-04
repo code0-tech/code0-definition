f505d7b1bfbba9f438401384adb47ae4234766bf
Raphael <raphael.goetz.7@gmail.com>
2026-03-04 12:36:01 +0100

    drop: removed unused code

--- crates/cli/src/analyser/core.rs
+++ crates/cli/src/analyser/core.rs
@@ -57,19 +57,6 @@ impl Analyser {
         self.index.has_data_type(identifier, except_id)
     }
 
-    pub fn generic_key_in_target(&self, key: &str, target: &str) -> bool {
-        let norm_target = target.to_ascii_lowercase();
-        self.data_types.iter().any(|dt| {
-            dt.definition_data_type
-                .identifier
-                .eq_ignore_ascii_case(&norm_target)
-                && dt
-                    .definition_data_type
-                    .generic_keys
-                    .contains(&key.to_string())
-        })
-    }
-
     pub fn null_field(&mut self, name: String, adt: &AnalysableDataType) {
         self.reporter.add(Diagnose::new(
             adt.definition_data_type.identifier.clone(),
